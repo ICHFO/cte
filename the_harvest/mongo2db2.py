e629de6f29663a9571990f52d63a47361f500f33
Hans Fonteyn <hans.fonteyn@infocura.be>
2020-04-24 10:23:37 +0200

    added mongo2db2.py

---
+++ the_harvest/mongo2db2.py
@@ -0,0 +1,22 @@
+import ibm_db
+from pymongo import MongoClient
+
+
+mongo = MongoClient('penny.infocura.lan')
+db2cstr = "DATABASE=cte;HOSTNAME=skye.infocura.lan;PORT=50001;PROTOCOL=TCPIP;UID=ic;PWD=icmaster"
+db2conn = ibm_db.connect(db2cstr, '', '')
+
+mcur = mongo.cte.vacancies.find()
+for item in mcur:
+    url = item.get("url")
+    html = item.get("source")
+    sql = "insert into cte.vacancy_raw (url, html) values (?, ?)"
+    stmt = ibm_db.prepare(db2conn, sql)
+    try:
+        param = url, html
+        ibm_db.execute(stmt, param)
+    except:
+        print("Transaction couldn't be completed:", ibm_db.stmt_errormsg())
+
+ibm_db.close(db2conn)
+mongo.close()

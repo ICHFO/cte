9dee6e61f56954b48c2d01f073cb2339caadb33f
Hans Fonteyn <hans.fonteyn@infocura.be>
2020-04-24 11:45:56 +0200

    mdified mongo2db2.py

--- the_harvest/mongo2db2.py
+++ the_harvest/mongo2db2.py
@@ -10,10 +10,11 @@ mcur = mongo.cte.vacancies.find()
 for item in mcur:
     url = item.get("url")
     html = item.get("source")
-    sql = "insert into cte.vacancy_raw (url, html) values (?, ?)"
+    sql = "insert into cte.vacancy_raw (site, url, html) values (?, ?, ?)"
+    site = item.get("site")
     stmt = ibm_db.prepare(db2conn, sql)
     try:
-        param = url, html
+        param = site, url, html
         ibm_db.execute(stmt, param)
     except:
         print("Transaction couldn't be completed:", ibm_db.stmt_errormsg())
